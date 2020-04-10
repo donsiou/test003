d44bc14f6c664edfc2c0c2971aef35c1f228b810
RymBou <34248198+RymBou@users.noreply.github.com>
2020-04-10 12:47:16 +0200

    this is a message

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 class Client:
 	def __init__(self,name):
 		self._name=name
+		
 	def getName(self): 
 		return self._name
 	def setName(self,val):
 		self._name=val
+	
+	def test(self):
+		print "hello world!"
